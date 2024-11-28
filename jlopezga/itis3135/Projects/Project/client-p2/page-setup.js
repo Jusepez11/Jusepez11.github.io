5aed35143ce69de555b864eb55e57d65e4ae71c1
Sebastian Lopez-Gallardo <jusepez90@gmail.com>
2024-11-28 15:08:34 -0500

    discontinuing it

--- jlopezga/itis3135/Projects/Project/client-p2/page-setup.js
+++ jlopezga/itis3135/Projects/Project/client-p2/page-setup.js
@@ -1,16 +1,22 @@
-const elements = [
-    document.getElementById('home'),
-    document.getElementById('about'),
-    document.getElementById('feedback'),
-    document.getElementById('hall-of-fame'),
-    document.getElementById('join-us')
-];
+document.addEventListener("DOMContentLoaded", function() {
+  const elements = [
+      document.getElementById('home'),
+      document.getElementById('about'),
+      document.getElementById('feedback'),
+      document.getElementById('hall-of-fame'),
+      document.getElementById('join-us')
+  ];
 
-function hiding(id){
-  elements.forEach((element) => {
-    element.classList.add('hidden');
+  elements[0].addEventListener('click', function(){
+    hiding(0);
   });
 
-  document.getElementById(id).classList.remove('hidden');
-}
+  function hiding(id){
+    elements.forEach((element) => {
+      element.classList.add('hidden');
+    });
 
+    document.getElementById([id]).classList.remove('hidden');
+  }
+
+});
