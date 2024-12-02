ee087a003e0b32bb5f01b64b3ee4ab3a41493fb1
Sebastian Lopez-Gallardo <jusepez90@gmail.com>
2024-12-02 16:30:26 -0500

    updated the js
    no ontarget

--- jlopezga/itis3135/Projects/JavaScript-Scripts/script.js
+++ jlopezga/itis3135/Projects/JavaScript-Scripts/script.js
@@ -1,76 +1,119 @@
-const polygon = {
-    1 : "henagon",
-    2 : "digon",
-    3 : "trigon",
-    4 : "tetragon",
-    5 : "pentagon",
-    6 : "hexagon",
-    7 : "heptagon",
-    8 : "octagon",
-    9 : "enneagon",
-    10 : "decagon",
-    11: "hendecagon"
-};
-
-function taxEvasion(){
-    const textElement = document.getElementById("buttons");
-    textElement.classList.remove("hidden");
-    textElement.innerHTML = "<p>Unfortunately the IRS took down this message.</p>";
+document.addEventListener("DOMContentLoaded", function() {
+    const polygon = {
+        1 : "henagon",
+        2 : "digon",
+        3 : "trigon",
+        4 : "tetragon",
+        5 : "pentagon",
+        6 : "hexagon",
+        7 : "heptagon",
+        8 : "octagon",
+        9 : "enneagon",
+        10 : "decagon",
+        11: "hendecagon"
+    };
     
-}
-
-function tip(){
-    const textElement = document.getElementById("buttons");
-    textElement.classList.remove("hidden");
-    textElement.innerHTML = "<p>Don't walk in front of a train.</p>";
-
-}
-
-function funFact(){
-    const textElement = document.getElementById("buttons");
-    textElement.classList.remove("hidden");
-    textElement.innerHTML = "<p>The honey badger is the most badass animal ever.</p>";
-
-}
-
-function randomQuote(){
-    const textElement = document.getElementById("buttons");
-    textElement.classList.remove("hidden");
-    textElement.innerHTML = "<p>Being a good example is the best form of service.</p>";
-
-}
-
-function easterEgg(){
-    const textElement = document.getElementById("buttons");
-    textElement.classList.remove("hidden");
-
-
-    document.getElementById("egg").innerHTML = "<h2 id='egg'>F1rst Scr1pts Page</h2>";
-
-}
-
-function submitText() {
-    const textElement = document.getElementById("text");
-    const name = document.getElementById("name").value; // Add .value to get the input value
-    const mood = document.getElementById("mood").value; // Add .value to get the input value
-
-    if (name && mood) {
+    function taxEvasion(){
+        const textElement = document.getElementById("buttons");
         textElement.classList.remove("hidden");
-        textElement.innerHTML = `<p>The Dinainca National Distributor welcomes you, ${name}! We're glad you are doing ${mood}!</p>`;
-    } else {
-        textElement.innerHTML = "<p>Please enter a name or mood based of what's missing.</p>";
+        textElement.innerHTML = "<p>Unfortunately the IRS took down this message.</p>";
+        
     }
-}
+    
+    function tip(){
+        const textElement = document.getElementById("buttons");
+        textElement.classList.remove("hidden");
+        textElement.innerHTML = "<p>Don't walk in front of a train.</p>";
+    
+    }
+    
+    function funFact(){
+        const textElement = document.getElementById("buttons");
+        textElement.classList.remove("hidden");
+        textElement.innerHTML = "<p>The honey badger is the most badass animal ever.</p>";
+    
+    }
+    
+    function randomQuote(){
+        const textElement = document.getElementById("buttons");
+        textElement.classList.remove("hidden");
+        textElement.innerHTML = "<p>Being a good example is the best form of service.</p>";
+    
+    }
+    
+    function easterEgg(){
+        const textElement = document.getElementById("buttons");
+        textElement.classList.remove("hidden");
+    
+    
+        document.getElementById("egg").innerHTML = "<h2 id='egg'>F1rst Scr1pts Page</h2>";
+    
+    }
+    
+    function submitText() {
+        const textElement = document.getElementById("text");
+        const name = document.getElementById("name").value; // Add .value to get the input value
+        const mood = document.getElementById("mood").value; // Add .value to get the input value
+    
+        if (name && mood) {
+            textElement.classList.remove("hidden");
+            textElement.innerHTML = `<p>The Dinainca National Distributor welcomes you, ${name}! We're glad you are doing ${mood}!</p>`;
+        } else {
+            textElement.innerHTML = "<p>Please enter a name or mood based of what's missing.</p>";
+        }
+    }
+    
+    function showPolygon() {
+        const polygonElement = document.getElementById("polygon");
+        polygonElement.classList.remove("hidden");
+        
+        const numberInput = document.getElementById("number").value; // Add .value to get the input value
+        
+        if (numberInput >= 1 && numberInput <= 11) {
+            polygonElement.innerHTML = `<p>Your polygon is a ${polygon[numberInput]}!</p>`;
+        } else {
+            polygonElement.innerHTML = "<p>Please enter a number between 1 and 11.</p>";
+        }
+    }
 
-function showPolygon() {
-    const polygonElement = document.getElementById("polygon");
-    polygonElement.classList.remove("hidden");
-    
-    const numberInput = document.getElementById("number").value; // Add .value to get the input value
-    
-    if (numberInput >= 1 && numberInput <= 11) {
-        polygonElement.innerHTML = `<p>Your polygon is a ${polygon[numberInput]}!</p>`;
-    } else {
-        polygonElement.innerHTML = "<p>Please enter a number between 1 and 11.</p>";
-    }
-}
+    const submitTextId = document.getElementById("submit-text");
+    const showPolygonId = document.getElementById("show-polygon");
+    const taxEvasionId = document.getElementById("tax-evasion");
+    const tipId = document.getElementById("tip");
+    const funFactId = document.getElementById("fun-fact");
+    const randomQuoteId = document.getElementById("random-quote");
+    const easterEggId = document.getElementById("easter-egg");
+
+
+    submitTextId.addEventListener("click", function(){
+        submitText();    
+    });
+
+    showPolygonId.addEventListener("click", function(){
+        showPolygon();    
+    });
+
+    taxEvasionId.addEventListener("click", function(){
+        taxEvasion();    
+    });
+
+    tipId.addEventListener("click", function(){
+        tip();    
+    });
+
+    funFactId.addEventListener("click", function(){
+        funFact();    
+    });
+
+    randomQuoteId.addEventListener("click", function(){
+        randomQuote();    
+    });
+
+    easterEggId.addEventListener("click", function(){
+        easterEgg();    
+    });
+
+
+
+
+});
